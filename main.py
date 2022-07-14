62b6acd2a0c9090e7b2e7f58e1f7391a2597f9da
LAXYA9887 <lxya98874322688423@protonmail.com>
2022-07-14 15:37:48 +0800

    main

---
+++ main.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 14 15:06:51 2022
+
+@author: Cryptomero‧Zero
+"""
+
+from stripDOT import strip
+import option
+
+parameters = option.parameter_validate(option.prepare_parameters())
+
+inputFile = parameters.in_file
+targetCol = parameters.num
+symbol = parameters.symbol
+output = parameters.out_file
+
+strip(
+      fileDir = inputFile,
+      targetCol = targetCol,
+      stripSymbol = symbol,
+      outFile = output
+)
+
+print(
+      """
+      {} was process. \n
+      Gene accession ID without {} was recorded to {}
+      """.format(inputFile,symbol,output)
+)
