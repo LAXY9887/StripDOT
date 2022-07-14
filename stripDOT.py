4999549a327c5b556596ce24397b4fc72ed7a7b4
LAXYA9887 <lxya98874322688423@protonmail.com>
2022-07-14 15:40:09 +0800

    Strip function

--- stripDOT.py
+++ stripDOT.py
@@ -1,11 +1,11 @@
-# fileDir = input檔案
+# fileDir = input BED 檔案
 # targetCol = ID欄位預設=3(BED檔)
 # stripSymbol = 要切掉的字元預設"."，切掉.以後的字
-# outDirPrefix = output路徑/檔名，預設:當前資料夾/converted.txt
+# outFile = output路徑+檔名，預設:當前資料夾 ./converted.txt
 
 # 沒有用任何package，歡迎自行修改
 
-def strip(fileDir,targetCol=3,stripSymbol=".",outDirPrefix="./converted.txt"):
+def strip(fileDir,targetCol,stripSymbol,outFile):
     targrtfile = open(fileDir)
     context = targrtfile.readlines()
     targrtfile.close()
@@ -19,9 +19,10 @@ def strip(fileDir,targetCol=3,stripSymbol=".",outDirPrefix="./converted.txt"):
     for each in convert:
         converted.append(each.split(stripSymbol)[0])
 
-    newFile = open(outDirPrefix+"","w")
+    newFile = open(outFile,"w")
     for each in converted:
         newFile.write(each+"\n")
     newFile.close()
 
-strip("testFiles\mm10_UCSC_IGV_TERCup_TERCanti2anti3_H33_genes.bed",outDirPrefix="converted_mm10_UCSC_IGV_TERCup_TERCanti2anti3_H33_genes.txt")
+if __name__=="__main__":
+    print("Strip DOT of gene accession number.")
